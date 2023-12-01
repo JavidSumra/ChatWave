529a86ef665b1372ab354ab6b42dcfb6fde9cb72
JavidSumra <javidsumra135@gmail.com>
2023-12-01 12:31:33 +0530

    Changing URL for Socket Connection

--- src/context/socket.ts
+++ src/context/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://chatwavejavid.netlify.app/"
+    ? "https://chatwave-tulv.onrender.com"
     : "http://localhost:3001";
 
 const socket = io(URL);
